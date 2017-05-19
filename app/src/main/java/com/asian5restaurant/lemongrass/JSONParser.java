d529600311756307a2951b7a1b8fb0d8d3abc048
amalsajuvarghese <amal@webqua.com>
2017-05-19 12:47:56 +0530

    completed app

--- app/src/main/java/com/asian5restaurant/lemongrass/JSONParser.java
+++ app/src/main/java/com/asian5restaurant/lemongrass/JSONParser.java
@@ -26,7 +26,7 @@ public class JSONParser {
     static InputStream is = null;
     static JSONObject jObj = null;
     static String json = "";
- 
+  
     // constructor
     public JSONParser() {
  
